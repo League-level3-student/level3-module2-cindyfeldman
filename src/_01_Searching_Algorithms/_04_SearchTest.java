e5e091dd2b5bc143951f2b966aa5b988e0c98811
cindyfeldman <league@WTS-iMac-11.attlocal.net>
2019-03-17 11:26:20 -0700

    SearchTest

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -10,24 +10,42 @@ class _04_SearchTest {
 	
 	@Test
 	public void testLinearSearch() {
+		_00_LinearSearch linearSearch = new _00_LinearSearch();
+		String[] words  = {
+				"hi","bye","whats up"
+		};
 		//1. use the assertEquals method to test your linear search method.
+	assertEquals(linearSearch.linearSearch(words, "hi"),0);
+	
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		_01_BinarySearch binarySearch = new _01_BinarySearch();
+		int[] array = {1,2,3,4};
+		assertEquals(binarySearch.binarySearch(array, 1, 4, 1),0);
+		assertEquals(binarySearch.binarySearch(array, 1, 4, 2),-1);
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		_02_InterpolationSearch interpolationSearch = new _02_InterpolationSearch();
+		int[] array = {1,2,3,4};
+		
+		assertEquals(interpolationSearch.interpolationSearch(array, 1),0);
+		assertEquals(interpolationSearch.interpolationSearch(array, 2),1);
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		_03_ExponentialSearch exponentialSearch = new _03_ExponentialSearch();
+		int[] array = {1,2,3,4,};
+		assertEquals(exponentialSearch.exponentialSearch(array, 1),0);
 	}
 }
